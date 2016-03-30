f70d93f807633a56e2e897fed94806cabe794e42
resteve <resteve@zikzakmedia.com>
2016-03-30 10:44:06 +0200

    Reactive RST tests scenarios

--- tests/test_stock_shipment_update_product_cost_price.py
+++ tests/test_stock_shipment_update_product_cost_price.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class StockShipmentUpdateProductCostPriceTestCase(ModuleTestCase):
@@ -20,5 +21,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_stock_average_cost_price.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
